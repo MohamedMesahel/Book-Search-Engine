8f86197cc8d2aa3994c57f86c4c2666d11f2738f
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-10-04 15:42:49 -0700

    setting up queryjs

---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+// TODO: import GraphQL from apollo
+import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
