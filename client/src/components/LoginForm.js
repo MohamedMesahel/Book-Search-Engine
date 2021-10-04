9d018b07edfe4ac90583c4a1a899a438b77fc0d4
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-10-04 15:33:32 -0700

    import mutations for loginjs and building mutatios files

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,13 +2,16 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
+// TODO: import hooks and functions 
+import { useMutation } from '@apollo/react-hooks';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [loginUser] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
---
+++ client/src/utils/mutations.js
@@ -0,0 +1,77 @@
+import { gql } from '@apollo/client';
+// TODO: Construct login credentials and mutation
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+// TODO: Cosntruct add user mutation
+export const ADD_USER = gql`
+mutation addUser($username: String!, $password: String!, $email: String!) {
+  addUser(username: $username, password: $password, email: $email) {
+    
+    user {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        authors
+        bookId
+        image
+        link
+        title
+        description
+      }
+    }
+    token
+  }
+}
+`;
+// TODO: Cosntruct save book mutation
+export const SAVE_BOOK = gql`
+
+    mutation saveBook($input: savedBook!) {
+    saveBook (input: $input)
+        {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                image
+                link
+                title
+                description
+            }
+        }
+    }
+`;
+
+// TODO: Cosntruct remove book mutation
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId:$bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                image
+                link
+                title
+                description
+            }
+        }
+}
+`;
