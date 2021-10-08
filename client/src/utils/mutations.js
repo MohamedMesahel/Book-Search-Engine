af7460078e57b4ad1bd75ae1c9580983f94786d3
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-10-07 20:22:10 -0700

    debuging and getting apollo client

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,23 +1,12 @@
-// import { gql } from '@apollo/client';
-import gql from 'graphql-tag';
-
 // TODO: Construct login credentials and mutation
-export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
-    }
-  }
-`;
+import { gql } from '@apollo/client';
+// import gql from 'graphql-tag';
+
 // TODO: Cosntruct add user mutation
 export const ADD_USER = gql`
 mutation addUser($username: String!, $password: String!, $email: String!) {
   addUser(username: $username, password: $password, email: $email) {
-    
+    token
     user {
       _id
       username
@@ -32,11 +21,22 @@ mutation addUser($username: String!, $password: String!, $email: String!) {
         description
       }
     }
-    token
   }
 }
 `;
-// TODO: Cosntruct save book mutation
+// TODO: Add login mutation
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+// TODO: Add saveBook mutation
 export const SAVE_BOOK = gql`
 
     mutation saveBook($input: SavedBookInput!) {
