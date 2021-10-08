554199908306633f069f58e8b9a442f0933f95ab
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-10-07 20:14:30 -0700

    using apolloclient in appjs

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
-// import {
-//   ApolloClient, ApolloProvider, InMemoryCache, createHttpLink} from '@apollo/client';
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  ApolloProvider,
+  // InMemoryCache,
+  // createHttpLink,
+} from '@apollo/client';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import { setContext } from '@apollo/client/link/context';
+// import { ApolloProvider } from '@apollo/react-hooks';
+// import ApolloClient from 'apollo-boost';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
@@ -21,26 +26,6 @@ const client = new ApolloClient({
   },
   uri: '/graphql',
 });
-// // Construct our main GraphQL API endpoint
-// const httpLink = createHttpLink({
-//   uri: '/graphql',
-// });
-// const authLink = setContext((_, { headers }) => {
-//   // get the authentication token from local storage if it exists
-//   const token = localStorage.getItem('id_token');
-//   // return the headers to the context so httpLink can read them
-//   return {
-//     headers: {
-//       ...headers,
-//       authorization: token ? `Bearer ${token}` : '',
-//     },
-//   };
-// });
-// const client = new ApolloClient({
-//   // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
-//   link: authLink.concat(httpLink),
-//   cache: new InMemoryCache(),
-// });
 
 function App() {
   return (
@@ -61,3 +46,23 @@ function App() {
 }
 
 export default App;
+// // Construct our main GraphQL API endpoint
+// const httpLink = createHttpLink({
+//   uri: '/graphql',
+// });
+// const authLink = setContext((_, { headers }) => {
+//   // get the authentication token from local storage if it exists
+//   const token = localStorage.getItem('id_token');
+//   // return the headers to the context so httpLink can read them
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// });
+// const client = new ApolloClient({
+//   // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache(),
+// });
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,6 +1,6 @@
 // TODO: import GraphQL from apollo
-// import { gql } from '@apollo/client';
-import gql from 'graphql-tag';
+import { gql } from '@apollo/client';
+// import gql from 'graphql-tag';
 
 
 export const QUERY_ME = gql`
