558ef4ae6a591e5034a05dd9ca6cad53268e9922
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-10-10 14:28:49 -0700

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bookDB', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/book-search-enginem', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
