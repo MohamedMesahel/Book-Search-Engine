5c578127c319b02e6b1e9375257b7002087678ca
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-10-09 20:28:22 -0700

    edits to the package json

--- server/config/connection.js
+++ server/config/connection.js
@@ -8,3 +8,4 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bookDB', {
 });
 
 module.exports = mongoose.connection;
+// mongodb+srv://Root:Zizo1234@cluster0.tjgsv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
--- server/server.js
+++ server/server.js
@@ -1,24 +1,4 @@
 
-// import { ApolloServer } from 'apollo-server-express';
-// import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
-// import express from 'express';
-// import http from 'http';
-// const path = require('path');
-// const { typeDefs, resolvers } = require('./schemas');
-
-// async function startApolloServer(typeDefs, resolvers) {
-//   const app = express();
-//   const httpServer = http.createServer(app);
-//   const server = new ApolloServer({
-//     typeDefs,
-//     resolvers,
-//     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-//   });
-//   await server.start();
-//   server.applyMiddleware({ app });
-//   await new Promise(resolve => httpServer.listen({ port: 3001 }, resolve));
-//   console.log(` Server ready at http://localhost:4000${server.graphqlPath}`);
-// }
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
@@ -58,3 +38,25 @@ db.once('open', () => {
   });
 });
 
+// import { ApolloServer } from 'apollo-server-express';
+// import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+// import express from 'express';
+// import http from 'http';
+// const path = require('path');
+// const { typeDefs, resolvers } = require('./schemas');
+
+// async function startApolloServer(typeDefs, resolvers) {
+//   const app = express();
+//   const httpServer = http.createServer(app);
+//   const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+//   });
+//   await server.start();
+//   server.applyMiddleware({ app });
+//   await new Promise(resolve => httpServer.listen({ port: 3001 }, resolve));
+//   console.log(` Server ready at http://localhost:4000${server.graphqlPath}`);
+        // "heroku-postbuild": "NPM_CONFIG_PRODUCTION=false npm install --prefix client && npm run build --prefix client"
+
+// }
